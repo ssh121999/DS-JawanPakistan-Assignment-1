e0aed380f0e9c2d944d95fa9a3010e8da087c4db
Saad <hussainsaad121999@gmail.com>
2021-12-29 18:35:17 +0500

    assignments

---
+++ 5.py
@@ -0,0 +1,3 @@
+f_name = input("Input your first name : ")
+l_name = input("Input your last name : ")
+print(str(l_name) + " " + str(f_name))
---
+++ 6.py
@@ -0,0 +1,4 @@
+num1 = input("Input first number : ")
+num2 = input("Input second number : ")
+add = int(num1) + int(num2)
+print(str(num1) + " + " + str(num2) + " = " + str(add))
